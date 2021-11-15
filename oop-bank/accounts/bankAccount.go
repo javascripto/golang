e4e135600c5e9fb45201481451cf19121c9c43fe
Yuri Almeida <yurialves2@gmail.com>
2021-11-15 20:24:33 -0300

    make ballance private

--- oop-bank/accounts/bankAccount.go
+++ oop-bank/accounts/bankAccount.go
@@ -9,12 +9,12 @@ import (
 type BankAccount struct {
 	Number  int
 	Owner   client.Client
-	Balance float64
+	balance float64
 }
 
 func (account *BankAccount) Deposit(value float64) error {
 	if value > 0 {
-		account.Balance += value
+		account.balance += value
 		return nil
 	}
 	return errors.New("you can't, deposit a negative value")
@@ -22,14 +22,14 @@ func (account *BankAccount) Deposit(value float64) error {
 
 func (account *BankAccount) Withdraw(value float64) error {
 	if value > 0 {
-		account.Balance -= value
+		account.balance -= value
 		return nil
 	}
 	return errors.New("you can't withdraw a negative value")
 }
 
 func (account *BankAccount) Transfer(value float64, destination *BankAccount) error {
-	if value < 0 || account.Balance < value {
+	if value < 0 || account.balance < value {
 		return errors.New("account balance is not enough")
 	}
 	err := account.Withdraw(value)
@@ -43,12 +43,16 @@ func (account *BankAccount) Transfer(value float64, destination *BankAccount) er
 	return nil
 }
 
+func (account BankAccount) GetBalance() float64 {
+	return account.balance
+}
+
 // String method needs to reference the struct value, not its pointer
 func (account BankAccount) String() string {
 	return fmt.Sprintf(
-		`{"Account": %d, "Owner": "%s", "Balance": %.2f}`,
+		`{"Account": %d, "Owner": "%s", "balance": %.2f}`,
 		account.Number,
 		account.Owner,
-		account.Balance,
+		account.balance,
 	)
 }
--- oop-bank/main.go
+++ oop-bank/main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	account1 := accounts.BankAccount{Number: 1, Owner: client.Client{Name: "John"}, Balance: 100}
-	account2 := accounts.BankAccount{Number: 1, Owner: client.Client{Name: "Mary"}, Balance: 300}
+	account1 := accounts.BankAccount{Number: 1, Owner: client.Client{Name: "John"}}
+	account2 := accounts.BankAccount{Number: 1, Owner: client.Client{Name: "Mary"}}
+	account1.Deposit(500)
+	account2.Deposit(500)
 	account1.Transfer(50, &account2)
 	fmt.Println(account1)
 	fmt.Println(account2)
